4165b3092bcc09db6927df58d03fa6fd99497c31
Alex Braga <bragadeveloper@gmail.com>
2023-04-03 18:48:41 -0300

    primeiros testes

---
+++ src/app/pages/cart/cart.component.spec.ts
@@ -0,0 +1,117 @@
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { CartComponent } from "./cart.component";
+import { BookService } from "src/app/services/book.service";
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from "@angular/core";
+import { Book } from "src/app/models/book.model";
+
+const listBook: Book[] = [
+        {
+            name: '',
+            author: '',
+            isbn: '',
+            price: 15,
+            amount: 2,
+        },
+        {
+            name: '',
+            author: '',
+            isbn: '',
+            price: 8,
+            amount: 5,
+        },
+        {
+            name: '',
+            author: '',
+            isbn: '',
+            price: 28,
+            amount: 1,
+        }
+    ];
+
+describe('CartComponent', () => {
+
+    let component: CartComponent;
+    let fixture: ComponentFixture<CartComponent>;
+    let service: BookService;
+
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            imports: [
+                HttpClientTestingModule
+            ],
+            declarations: [ 
+                CartComponent 
+            ],
+            providers: [
+                BookService
+            ],
+            schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA]
+          }).compileComponents();
+    });
+
+    beforeEach(() => {
+        fixture = TestBed.createComponent(CartComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+        service = fixture.debugElement.injector.get(BookService);
+    });
+
+    it("should create", () => {
+        expect(component).toBeTruthy();
+    });
+
+    it('getTotalPrice returns an amount', () => {
+        const totalPrice = component.getTotalPrice(listBook);
+        expect(totalPrice).toBeGreaterThan(0);
+        expect(totalPrice).not.toBe(0);
+        expect(totalPrice).not.toBeNull();
+    });
+
+    it('onInputNumberChange increments correctly', () => {
+        const action = 'plus';
+        const book = {
+            name: '',
+            author: '',
+            isbn: '',
+            price: 15,
+            amount: 2,
+        };
+
+
+        
+        const spy1 = spyOn(service, 'updateAmountBook').and.callFake( () => null);
+        const spy2 = spyOn(component, 'getTotalPrice').and.callFake( () => null)
+
+        expect(book.amount).toBe(2);
+
+        component.onInputNumberChange(action, book);
+
+        expect(book.amount === 3).toBeTrue();
+
+        expect(spy1).toHaveBeenCalled();
+        expect(spy2).toHaveBeenCalled();
+    });
+
+    it('onInputNumberChange decrements correctly', () => {
+        const action = 'minus';
+        const book = {
+            name: '',
+            author: '',
+            isbn: '',
+            price: 15,
+            amount: 3,
+        };
+        
+        expect(book.amount).toBe(3);
+        const spy1 = spyOn(service, 'updateAmountBook').and.callFake( () => null);
+        const spy2 = spyOn(component, 'getTotalPrice').and.callFake( () => null);
+
+        component.onInputNumberChange(action, book);
+        expect(book.amount).toBe(2);
+        expect(spy1).toHaveBeenCalled();
+        expect(spy2).toHaveBeenCalled();
+    });
+
+
+});
